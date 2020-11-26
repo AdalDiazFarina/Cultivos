60d00b7b50e6d8d47e172cbb43200b119fefc848
Adal Díaz Fariña <alu0101112251@ull.edu.es>
2020-11-26 15:16:40 +0000

    Nuevo atributo tipo de grano

--- lib/cultivos/cereal.rb
+++ lib/cultivos/cereal.rb
@@ -1,5 +1,7 @@
 class Cereal < Cultivo
-    def initialize(name, description, gei, terr, price, cost)
+    attr_reader :tipo_grano
+    def initialize(name, description, gei, terr, price, cost, tipo_grano)
         super(name, description, gei, terr, price, cost)
+        @tipo_grano = tipo_grano
     end
 end
